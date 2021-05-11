27980789651d4b94c9b1844eb3074cd5de021446
lishuai <1625396281@qq.com>
2021-05-11 18:34:47 +0800

    idea 第三次提交

--- src/main/java/cn/htu/GitDemoTest.java
+++ src/main/java/cn/htu/GitDemoTest.java
@@ -4,5 +4,6 @@ public class GitDemoTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello GitHub");
+        System.out.println("hello GitEE");
     }
 }
