8d78bdc09f1bd2954537231c1d95ad6c4e0c906c
lishuai <1625396281@qq.com>
2021-05-11 18:34:00 +0800

    idea 第二次提交

--- src/main/java/cn/htu/GitDemoTest.java
+++ src/main/java/cn/htu/GitDemoTest.java
@@ -3,5 +3,6 @@ package cn.htu;
 public class GitDemoTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello GitHub");
     }
 }
