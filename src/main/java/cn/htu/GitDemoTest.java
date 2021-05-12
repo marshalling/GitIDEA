c157177bd3e2b6fb40a20e6e2a6be1397c9a5678
lishuai110 <41361970+lishuai110@users.noreply.github.com>
2021-05-13 00:15:40 +0800

    Update GitDemoTest.java

--- src/main/java/cn/htu/GitDemoTest.java
+++ src/main/java/cn/htu/GitDemoTest.java
@@ -8,6 +8,7 @@ public class GitDemoTest {
         System.out.println("hello hot-fix");
         System.out.println("hello master conflict");
         System.out.println("hello hot-fix conflict");
+         System.out.println("pull test idea");
 
     }
 }
